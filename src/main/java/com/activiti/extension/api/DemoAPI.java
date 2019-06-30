ff5e6488131b8c495974e07e001ba44017ce8c29
tapan-d-thakkar <tapan.d.thakkar@gmail.com>
2019-06-30 12:43:37 -0700

    APS API added

--- src/main/java/com/activiti/extension/api/DemoAPI.java
+++ src/main/java/com/activiti/extension/api/DemoAPI.java
@@ -1,7 +1,5 @@
 package com.activiti.extension.api;
 
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.Api;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,10 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping(value = "/enterprise/demo", produces = MediaType.APPLICATION_JSON_VALUE)
-@Api(tags = { "demo" }, description = "Demo API")
+//@Api(tags = { "demo" }, description = "Demo API")
 public class DemoAPI {
 
-    @ApiOperation(value = "Demo API")
+//    @ApiOperation(value = "Demo API")
     @RequestMapping(value = "/test", method = RequestMethod.GET)
     public String sampleTestApi() {
         return "Demo API Response";
